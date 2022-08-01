4e7c873e7b5c5941463b1be7c4e9210abef3222a
Aminat Moldalieva <mldlv.mnt.kg@gmail.com>
2022-08-01 17:08:39 +0200

    kind of tryinied to refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hajj_app/MainPage.dart';
+import '../features/dashboard/presentation/screen/main_page.dart';
 
 void main() {
   runApp(const MyApp());
