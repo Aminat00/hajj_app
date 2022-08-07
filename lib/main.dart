c58352342e96e1a4c0ca555ceee08f0e2a61acde
Aminat Moldalieva <mldlv.mnt.kg@gmail.com>
2022-08-07 03:04:12 +0200

    firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,13 @@ import 'package:hajj_app/MainPage.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
+   MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
