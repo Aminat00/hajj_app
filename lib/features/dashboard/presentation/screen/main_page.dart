db5b3afa8a713f589cd329d9f39de4e5434f1f51
Aminat Moldalieva <mldlv.mnt.kg@gmail.com>
2022-08-08 20:02:04 +0200

    signin and signup page authorisation with firebase

--- lib/features/dashboard/presentation/screen/main_page.dart
+++ lib/features/dashboard/presentation/screen/main_page.dart
@@ -1,75 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:hajj_app/features/dashboard/presentation/screen/signin_screen.dart';
+
 import 'home.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({Key? key}) : super(key: key);
 
-
-  final String title;
-
-
-
+  final String title = 'Tawaf';
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _selectedIndex=0;
+  int _selectedIndex = 0;
   bool show404 = false;
 
-  static const List<Widget> _items =[
+  static const List<Widget> _items = [
     Home(),
-    Text('Index1: "Location"'),
+    Text('Index2: "Tasks"'),
     Text('Index2: "Tasks"'),
   ];
 
-  void _onTap(int index)
-  {
-    _selectedIndex=index;
-    setState((){
-
-    });
+  void _onTap(int index) {
+    _selectedIndex = index;
+    setState(() {});
   }
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       // extendBodyBehindAppBar: true,
       appBar: AppBar(
         // backgroundColor: const Color(0x44000000),
         backgroundColor: Colors.teal,
         elevation: 10,
-        title: Text(widget.title,
-        style: Theme.of(context).textTheme.headline2),
+        title: Text(widget.title, style: Theme.of(context).textTheme.headline2),
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const SignInScreen(),
+                  ),
+                );
+              },
+              icon: const Icon(Icons.account_circle_outlined, color: Colors.white,)),
+        ],
       ),
-      body: IndexedStack(
-            index:_selectedIndex,
-            children:_items
-        ),
+      body: IndexedStack(index: _selectedIndex, children: _items),
 
       bottomNavigationBar: showBottomNav(),
     );
   }
 
-  Widget showBottomNav()
-  {
+  Widget showBottomNav() {
     return BottomNavigationBar(
       backgroundColor: const Color(0xFF8AD7C7),
       items: const [
         BottomNavigationBarItem(
           icon: Icon(Icons.home),
-          label:'Home',
+          label: 'Home',
         ),
         BottomNavigationBarItem(
           icon: Icon(
             Icons.location_on,
           ),
-          label:'Places',
+          label: 'Places',
         ),
         BottomNavigationBarItem(
           icon: Icon(Icons.task_alt),
-          label:'Agenda',
+          label: 'Agenda',
         ),
       ],
       currentIndex: _selectedIndex,
@@ -79,5 +81,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
-
