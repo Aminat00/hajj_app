419beb3c81e02e2a54aed8255c0e5f307b95b981
Dima <dimagolt@gmail.com>
2022-08-09 18:30:13 +0200

    add hijri date to main screen

--- lib/features/dashboard/presentation/widget/prayer_times.dart
+++ lib/features/dashboard/presentation/widget/prayer_times.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hajj_app/models/hijri_date.dart';
 import 'package:hajj_app/services/prayers_service.dart';
 import 'package:hajj_app/models/prayers.dart';
+import 'package:hajj_app/utils/latinize.dart';
 import 'prayer_time_icon.dart';
 import 'package:intl/intl.dart';
 
@@ -12,24 +14,24 @@ class PrayerTimes extends StatefulWidget {
 }
 
 class _PrayerTimesState extends State<PrayerTimes> {
-  late Future<Prayers?> _futurePrayers;
+  late Future<Pair?> _futurePair;
 
   @override
   void initState() {
-    _futurePrayers = PrayersService.fetchPrayers();
+    _futurePair = PrayersService.fetchPair();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: _futurePrayers,
+        future: _futurePair,
         builder: (context, snapshot) {
           if (snapshot.hasError) {
             return Text("Error fetching prayers: ${snapshot.error.toString()}");
           } else if (snapshot.hasData) {
-            if (snapshot.data is Prayers) {
-              return _prayers(snapshot.data as Prayers);
+            if (snapshot.data is Pair) {
+              return _prayers(snapshot.data as Pair);
             } else {
               return const CircularProgressIndicator();
             }
@@ -39,39 +41,49 @@ class _PrayerTimesState extends State<PrayerTimes> {
         });
   }
 
-  _prayers(Prayers prayers) {
+  _prayers(Pair pair) {
     DateFormat dateFormat = DateFormat("H:mm");
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          PrayerTimeIcon(
-              salahName: 'Fajr',
-              salahIcon: Icons.brightness_4,
-              salahTime: dateFormat.format(prayers.Fajr.toLocal())),
-          PrayerTimeIcon(
-              salahName: 'Sunrise',
-              salahIcon: Icons.brightness_5,
-              salahTime: dateFormat.format(prayers.Sunrise.toLocal())),
-          PrayerTimeIcon(
-              salahName: 'Dhuhr',
-              salahIcon: Icons.wb_sunny,
-              salahTime: dateFormat.format(prayers.Dhuhr.toLocal())),
-          PrayerTimeIcon(
-              salahName: 'Asr',
-              salahIcon: Icons.wb_twilight,
-              salahTime: dateFormat.format(prayers.Asr.toLocal())),
-          PrayerTimeIcon(
-              salahName: 'Maghrib',
-              salahIcon: Icons.wb_twighlight,
-              salahTime: dateFormat.format(prayers.Maghrib.toLocal())),
-          PrayerTimeIcon(
-              salahName: 'Isha',
-              salahIcon: Icons.bedtime,
-              salahTime: dateFormat.format(prayers.Isha.toLocal())),
-        ],
-      ),
+    final Prayers prayers = pair.item1 as Prayers;
+    final HijriDate hijriDate = pair.item2 as HijriDate;
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text('${hijriDate.day} ${latinize(hijriDate.month)} ${hijriDate.year}'),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              PrayerTimeIcon(
+                  salahName: 'Fajr',
+                  salahIcon: Icons.brightness_4,
+                  salahTime: dateFormat.format(prayers.Fajr.toLocal())),
+              PrayerTimeIcon(
+                  salahName: 'Sunrise',
+                  salahIcon: Icons.brightness_5,
+                  salahTime: dateFormat.format(prayers.Sunrise.toLocal())),
+              PrayerTimeIcon(
+                  salahName: 'Dhuhr',
+                  salahIcon: Icons.wb_sunny,
+                  salahTime: dateFormat.format(prayers.Dhuhr.toLocal())),
+              PrayerTimeIcon(
+                  salahName: 'Asr',
+                  salahIcon: Icons.wb_twilight,
+                  salahTime: dateFormat.format(prayers.Asr.toLocal())),
+              PrayerTimeIcon(
+                  salahName: 'Maghrib',
+                  salahIcon: Icons.wb_twighlight,
+                  salahTime: dateFormat.format(prayers.Maghrib.toLocal())),
+              PrayerTimeIcon(
+                  salahName: 'Isha',
+                  salahIcon: Icons.bedtime,
+                  salahTime: dateFormat.format(prayers.Isha.toLocal())),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/services/prayers_service.dart
+++ lib/services/prayers_service.dart
@@ -1,20 +1,31 @@
 import 'dart:convert';
 
+import 'package:hajj_app/models/hijri_date.dart';
 import 'package:hajj_app/models/prayers.dart';
 import 'package:http/http.dart' as http;
 
 class PrayersService {
-  static Future<Prayers?> fetchPrayers() async {
+  static Future<Pair?> fetchPair() async {
     http.Response response = await http.get(Uri.parse(
         'http://api.aladhan.com/v1/timingsByCity?city=Warsaw&country=Poland&iso8601=true'));
     if (response.statusCode == 200) {
       Map prayersData = jsonDecode(response.body);
       Map<String, dynamic> prayers = prayersData['data']['timings'];
+      Map<String, dynamic> hijriDate = prayersData['data']['date']['hijri'];
       final mappedPrayers = Prayers.fromJson(prayers);
-      return mappedPrayers;
+      final mappedHijriDate = HijriDate.fromJson(hijriDate);
+      final pair = Pair(mappedPrayers, mappedHijriDate);
+      return pair;
     } else {
       print("Error with code ${response.statusCode}");
       return null;
     }
   }
+}
+
+class Pair {
+  final Object item1;
+  final Object item2;
+
+  Pair(this.item1, this.item2);
 }
