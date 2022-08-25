7f0aaaf2f878d3c95fd468653cd1da43e4118a5a
Dima <dimagolt@gmail.com>
2022-08-25 19:09:45 +0200

    change umrah screen

--- lib/features/dashboard/presentation/screen/umrah_screen.dart
+++ lib/features/dashboard/presentation/screen/umrah_screen.dart
@@ -40,20 +40,26 @@ class _UmrahScreenState extends State<UmrahScreen> {
         future: _futureUmrah,
         builder: (context, snapshot) {
           if (snapshot.hasError) {
-            return Text(
-                "Error fetching umrah processes: ${snapshot.error.toString()}");
+            return Text("Error fetching umrah processes: ${snapshot.error.toString()}");
           } else if (snapshot.hasData) {
             if (snapshot.data is UmrahList) {
               final UmrahList list = snapshot.data as UmrahList;
               return SingleChildScrollView(
                 child: Column(
                   children: [
-                    _umrah(list.prepare, 'Prepare'),
-                    _umrah(list.ihram, 'Ihram'),
-                    _umrah(list.tawafAlUmrah, 'Tawah al Umrah'),
-                    _umrah(list.sai, 'Sa\'i'),
-                    _umrah(list.halqOrTaqsir, 'Halq or Taqsir'),
-                    _umrah(list.mabrook, 'Mabrook!'),
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(context, MaterialPageRoute(builder: (context) {
+                            return UmrahScreen2(umrah: list.prepare, category: 'Prepare');
+                          }));
+                        },
+                        child: Text('Prepare')),
+                    // _umrah(list.prepare, 'Prepare'),
+                    // _umrah(list.ihram, 'Ihram'),
+                    // _umrah(list.tawafAlUmrah, 'Tawah al Umrah'),
+                    // _umrah(list.sai, 'Sa\'i'),
+                    // _umrah(list.halqOrTaqsir, 'Halq or Taqsir'),
+                    // _umrah(list.mabrook, 'Mabrook!'),
                   ],
                 ),
               );
@@ -67,52 +73,64 @@ class _UmrahScreenState extends State<UmrahScreen> {
       ),
     );
   }
+}
 
+class UmrahScreen2 extends StatelessWidget {
+  const UmrahScreen2({Key? key, required this.umrah, required this.category}) : super(key: key);
 
-  _umrah(List<Umrah> umrah, String category) {
-    return CustomScrollView(
-      physics: const NeverScrollableScrollPhysics(),
-      shrinkWrap: true,
-      slivers: [
-        SliverToBoxAdapter(
-          child: Align(
-          alignment:Alignment.center ,
-        child:Text(category, style: Theme
-            .of(context)
-            .textTheme
-            .headline5,),
-    )
-        ),
-        SliverList(
-          delegate: SliverChildBuilderDelegate(
-            (BuildContext context, int index) {
-              if (umrah[index].card_title != null) {
-                return _umrahCard(umrah[index], index);
-              } else {
-                return _umrahTile(umrah[index], index);
-              }
-            },
-            childCount: umrah.length,
+  final List<Umrah> umrah;
+  final String category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(category),
+      ),
+      body: CustomScrollView(
+        physics: const NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        slivers: [
+          SliverToBoxAdapter(
+            child: Align(
+              alignment: Alignment.center,
+              child: Text(
+                category,
+                style: Theme.of(context).textTheme.headline5,
+              ),
+            ),
           ),
-        ),
-      ],
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                if (umrah[index].card_title != null) {
+                  return _umrahCard(umrah[index], index, context);
+                } else {
+                  return _umrahTile(umrah[index], index, context);
+                }
+              },
+              childCount: umrah.length,
+            ),
+          ),
+        ],
+      ),
     );
   }
 
-  _umrahCard(Umrah umrah, int index) {
+  _umrahCard(Umrah umrah, int index, BuildContext context) {
     if (umrah.card_title != null && umrah.card_text != null) {
       return InkWell(
-          onTap:  () =>
-          Navigator.push(context, MaterialPageRoute(builder: (context) => UmrahStepsCard(umrah: umrah))),
-   child: umrahCard(umrah.card_title, umrah.card_text, umrah.card_img));
+          onTap: () => Navigator.push(
+              context, MaterialPageRoute(builder: (context) => UmrahStepsCard(umrah: umrah))),
+          child: umrahCard(umrah.card_title, umrah.card_text, umrah.card_img));
     }
   }
 
-  _umrahTile(Umrah umrah, int index) {
-    if (umrah.list_tile_text!=null) {
-      return  InkWell(
-          onTap:  () =>
-          Navigator.push(context, MaterialPageRoute(builder: (context) => UmrahStepsTile(umrah: umrah))),
+  _umrahTile(Umrah umrah, int index, BuildContext context) {
+    if (umrah.list_tile_text != null) {
+      return InkWell(
+          onTap: () => Navigator.push(
+              context, MaterialPageRoute(builder: (context) => UmrahStepsTile(umrah: umrah))),
           child: Card(
             color: Colors.white,
             elevation: 7.0,
@@ -120,15 +138,17 @@ class _UmrahScreenState extends State<UmrahScreen> {
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(15),
             ),
-        child: ListTile(
-          title: Text(umrah.list_tile_text!,
-          style: Theme.of(context).textTheme.bodyText1,),
-          trailing: const Icon(
-            Icons.arrow_forward,
-            color: Colors.teal,
-          ),
-        ),
-      ));
+            child: ListTile(
+              title: Text(
+                umrah.list_tile_text!,
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+              trailing: const Icon(
+                Icons.arrow_forward,
+                color: Colors.teal,
+              ),
+            ),
+          ));
     }
   }
 }
